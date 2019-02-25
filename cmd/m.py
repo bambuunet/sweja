b12bcc780226269fe9d24913f8affa042b50b75a
bambuunet <ringo@bambuu.net>
2019-02-25 09:39:52 +0900

    190225

--- cmd/m.py
+++ cmd/m.py
@@ -61,18 +61,20 @@ new_file.close()
 shutil.copyfile(TMP_FILE, NEW_FILE)
 os.remove(TMP_FILE)
 
-# remove prototype declaration, TLS, \r, \t
+
+# remove prototype declaration, TLS, \r, \t, //comment
 # change TRUE, FALSE
 new_file = open(NEW_FILE, 'r')
 lines = new_file.read()
 new_file.close()
 new_file = open(NEW_FILE, 'w')
+lines = re.sub(r'\r\n', "\n", lines)
 lines = re.sub(r'\n(static\s+|extern\s+)?\w+\s+\*?\w+\([^\)]+\)\s*;', "", lines)
 lines = re.sub(r'(\n|\s)TLS\s', '\\1', lines)
-lines = re.sub(r'\r', "", lines)
 lines = re.sub(r'\t', " ", lines)
 lines = re.sub(r'(?<!\w)TRUE(?!\w)', "true", lines)
 lines = re.sub(r'(?<!\w)FALSE(?!\w)', "false", lines)
+lines = re.sub(r'//[^\n]*\n', "", lines)
 new_file.write(lines)
 new_file.close()
 
@@ -108,25 +110,30 @@ new_file.close()
 shutil.copyfile(TMP_FILE, NEW_FILE)
 os.remove(TMP_FILE)
 
-
-
-
 # change struct to Map
+# change to 1 line
+new_file = open(NEW_FILE, 'r')
+lines = new_file.read()
+new_file.close()
+while True:
+  lines2 = re.sub(r'(struct\s+\w+\s*{[^\n}]*)\n+', "\\1", lines)
+  if lines == lines2:
+    break
+  else:
+    lines = lines2
+new_file = open(NEW_FILE, 'w')
+new_file.write(lines)
+new_file.close()
+
 new_file = open(NEW_FILE, 'r')
 for line in new_file:
-  # make const if "xxx" or (xxx) or number
-  define = re.match(r'#define\s+(\w+)\s+(struct\s+)?("[^"]*"|\([^\)]*\)|[\w\-\.]+)(.*)', line)
-  if define:
-    line = "const " + define.group(1) + " = " + define.group(3) + ";\n" + define.group(4)
-  define = re.match(r'#define\s+(\w+)\s+\{([^\}]*)\}(.*)', line)
-  if define:
-    line = "const " + define.group(1) + " = [" + define.group(2) + "];\n" + define.group(3)
-
-  # make function
-  define = re.match(r'#define\s+(\w+\([^\)]*\))\s+(.*)', line)
-  if define:
-    line = "function " + define.group(1) + "{return " + define.group(2) + "}"
-
+  if re.match(r'struct\s+\w+\s*{', line):
+    line = re.sub(r'\[[\w\+\* ]+\]', '', line)
+    line = re.sub(r',', ';', line)
+    line = re.sub(r'([{\s;]+)struct\s+\w+\s+\*?(\w+)\s*;', '\\1\\2;', line)
+    line = re.sub(r'([{\s;]+)\w+\s+\*?(\w+)\s*;', '\\1\\2;', line)
+    line = re.sub(r'({?)([\s;]+)(\w+);', '\\1["\\3",null],', line)
+    line = re.sub(r'struct\s+(\w+)\s*{([^}]*)}', 'var \\1 = new Map([\\2])', line)
   tmp_file = open(TMP_FILE,'a')
   tmp_file.write(line)
   tmp_file.close()
@@ -140,7 +147,7 @@ new_file = open(NEW_FILE, 'r')
 lines = new_file.read()
 new_file.close()
 new_file = open(NEW_FILE, 'w')
-lines = re.sub(r'\s*/\*([^\*]|\*[^/])*\*/', "", lines)
+lines = re.sub(r'\s*/\*([^/]|[^\*]/)*\*/', "", lines)
 lines = re.sub(r'\n+', "\n", lines)
 new_file.write(lines)
 new_file.close()
